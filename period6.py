f1ae048181143aacc2309e064696aa7943f1e296
billyjack0911 leffler <Willleffler@icloud.com>
2023-09-12 21:02:49 +0000

    Period 6 Task

--- period6.py
+++ period6.py
@@ -1,1 +1,22 @@
-print ["lucky charms"]
+restaurants = ["In N Out", "Panda Express", "Mcdonalds", "Subway", "Coldstone", "PHS Cafeteria"]
+
+new_res = input("What restuarant would you like to add to the list")
+
+def rank_res(new_res, res_list):
+
+    for i in range(len(res_list)):
+
+        rank = input("Do you like A)" + new_res + "more or B" + res_list[i] + "more? A/B")
+
+        if rank == "A":
+            res_list.insert(i, new_res)
+            return res_list
+     
+        elif rank == "B":
+            continue 
+
+res_list.append(new_res)
+return res_list
+
+print("Your new ranking of restaurants is" , rank_res(new_res, reslist))
+
